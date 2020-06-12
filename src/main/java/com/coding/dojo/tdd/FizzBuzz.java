bc72bec3417e4c524b1f798f93746e18ada15f16
gm10285 <gm10285@gmail.com>
2020-06-12 14:23:05 +0800

    impl FizzBuzz(require 1)

--- src/main/java/com/coding/dojo/tdd/FizzBuzz.java
+++ src/main/java/com/coding/dojo/tdd/FizzBuzz.java
@@ -1,4 +1,37 @@
 package com.coding.dojo.tdd;
 
+// https://codingdojo.org/kata/FizzBuzz/
+
 public class FizzBuzz {
+
+    public FizzBuzz() {
+
+    }
+
+
+    public String speak(int num) {
+        if (isModThreeAndFiveEqualZero(num)) {
+            return "FizzBuzz";
+        }
+        if (isModThreeEqualZero(num)) {
+            return "Fizz";
+        }
+        if (isModFiveEqualZero(num)) {
+            return "Buzz";
+        }
+        return "" + num;
+    }
+
+    private boolean isModThreeAndFiveEqualZero(int num) {
+        return isModThreeEqualZero(num) && isModFiveEqualZero(num);
+    }
+
+    private boolean isModThreeEqualZero(int num) {
+        return num % 3 == 0;
+    }
+
+    private boolean isModFiveEqualZero(int num) {
+        return num % 5 == 0;
+    }
+
 }
--- src/test/java/com/coding/dojo/tdd/FizzBuzzTest.java
+++ src/test/java/com/coding/dojo/tdd/FizzBuzzTest.java
@@ -3,11 +3,54 @@ package com.coding.dojo.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 public class FizzBuzzTest {
 
     @Test
-    public void test_(){
+    public void test_speak_not_equal_3() {
+        final FizzBuzz fizzBuzz = new FizzBuzz();
 
+        int num = 1;
+
+        String result = fizzBuzz.speak(num);
+
+        assertThat(result).isEqualTo("" + num);
+
+    }
+
+    @Test
+    public void test_speak_Fizz() {
+        final FizzBuzz fizzBuzz = new FizzBuzz();
+
+        int num = 3;
+
+        String result = fizzBuzz.speak(num);
+
+        assertThat(result).isEqualTo("Fizz");
+
+    }
+
+    @Test
+    public void test_speak_Buzz() {
+        final FizzBuzz fizzBuzz = new FizzBuzz();
+
+        int num = 5;
+
+        String result = fizzBuzz.speak(num);
+
+        assertThat(result).isEqualTo("Buzz");
+    }
+
+    @Test
+    public void test_speak_FizzBuzz() {
+        final FizzBuzz fizzBuzz = new FizzBuzz();
+
+        int num = 15;
+
+        String result = fizzBuzz.speak(num);
+
+        assertThat(result).isEqualTo("FizzBuzz");
     }
 
 
