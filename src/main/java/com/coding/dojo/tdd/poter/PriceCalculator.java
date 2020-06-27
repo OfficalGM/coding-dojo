0d7eabe3075e90c93a5c81737c8b30d4548784b5
gm10285 <gm10285@gmail.com>
2020-06-27 21:02:15 +0800

    impl calculator

--- src/main/java/com/coding/dojo/tdd/poter/PriceCalculator.java
+++ src/main/java/com/coding/dojo/tdd/poter/PriceCalculator.java
@@ -9,7 +9,12 @@ public class PriceCalculator {
     }
 
     public double calculate(List<ShoppingItem> shoppingItemList) {
-        return shoppingItemList.size() * 8 * (1 - (shoppingItemList.size() - 1) * 0.05);
+        double totalPrice = 0;
+        for (ShoppingItem item : shoppingItemList) {
+            totalPrice += item.getQuantity() * item.getBook().getPrice();
+        }
+        double disCount = 1 - (shoppingItemList.size() - 1) * 0.05;
+        return totalPrice * disCount;
     }
 
 }
