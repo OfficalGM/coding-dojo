eea726569504f413d59f7362b6f0aa9c1f152a0a
gm10285 <gm10285@gmail.com>
2020-06-27 19:57:02 +0800

    add PriceCalculator Test

---
+++ src/main/java/com/coding/dojo/tdd/poter/PriceCalculator.java
@@ -0,0 +1,15 @@
+package com.coding.dojo.tdd.poter;
+
+import java.util.List;
+
+public class PriceCalculator {
+
+    public PriceCalculator() {
+
+    }
+
+    public Double calculate(List<Book> bookList) {
+        return null;
+    }
+
+}
---
+++ src/test/java/com/coding/dojo/tdd/poter/PriceCalculatorTest.java
@@ -0,0 +1,12 @@
+package com.coding.dojo.tdd.poter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PriceCalculatorTest {
+
+    @Test
+    public void calculate() {
+    }
+}
