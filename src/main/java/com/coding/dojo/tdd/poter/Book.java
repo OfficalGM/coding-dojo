b7c4ba605bf8e8af41603381b055720e1f51aa5d
gm10285 <gm10285@gmail.com>
2020-06-27 14:18:43 +0800

    add Book vo

--- src/main/java/com/coding/dojo/tdd/poter/Book.java
+++ src/main/java/com/coding/dojo/tdd/poter/Book.java
@@ -1,5 +1,15 @@
 package com.coding.dojo.tdd.poter;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 //TODO
+@AllArgsConstructor
+@Data
 public class Book {
+
+    private String name;
+
+    private Integer price;
+
 }
